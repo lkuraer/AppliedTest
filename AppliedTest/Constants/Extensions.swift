1014daa4ec4cfaca8c9c043ba52b9650087fc702
Ruslan <lkuraer@gmail.com>
2021-09-28 13:31:48 +0500

    final

---
+++ AppliedTest/Constants/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  AppliedTest
+//
+//  Created by Ruslan Sabirov on 9/28/21.
+//
+
+import Foundation
---
+++ AppliedTest/Xibs/SelectLocationVC/SelectLocationVC.swift
@@ -0,0 +1,29 @@
+//
+//  SelectLocationVC.swift
+//  AppliedTest
+//
+//  Created by Ruslan Sabirov on 9/28/21.
+//
+
+import UIKit
+
+class SelectLocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
